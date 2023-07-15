a161ccb5b93cf05ef6f2247dc5b8f1a0b3fb7919
devnetkid <jessefausett@gmail.com>
2023-07-15 15:43:25 -0600

    Added wireless options submenu

--- src/soutools/controller.py
+++ src/soutools/controller.py
@@ -45,8 +45,16 @@ def view_organization():
         logger.debug(f"Notified user that organization name is set to {org_name}")
     PromptUtils(Screen()).enter_to_continue()
 
-def wireless_options():
-    print("You have choosen select wireless")
+def report_of_wireless():
+    print("You have choosen report_of_wireless")
+    PromptUtils(Screen()).enter_to_continue()
+
+def report_wireless_with_ssid():
+    print("You have choosen select report_wireless_with_ssid")
+    PromptUtils(Screen()).enter_to_continue()
+
+def replace_radius_settings():
+    print("You have choosen replace_radius_settings")
     PromptUtils(Screen()).enter_to_continue()
 
 def menu():
@@ -54,12 +62,23 @@ def menu():
     # Create the root menu
     menu = ConsoleMenu(MENU_TITLE, color("Main menu screen", fg='Red'), show_exit_option=False)
     logger.debug('The ConsoleMenu has been instantiated')
+
+    # Create the wireless submenu
+    wireless_submenu = ConsoleMenu(MENU_TITLE, color("Wireless Submenu", fg='Red'), show_exit_option=False)
+    wireless_submenu.append_item(FunctionItem(color("Generate a report of all sites of type wireless", fg='Green'), report_of_wireless))
+    wireless_submenu.append_item(FunctionItem(color("Generate a report of all sites with a particular SSID", fg='Green'), report_wireless_with_ssid))
+    wireless_submenu.append_item(FunctionItem(color("Replace radius servers of a particular SSID", fg='Green'), replace_radius_settings))
+    wireless_submenu.append_item(ExitItem(color("Return to the main menu", fg='Green')))
+    # Menu item for opening submenu 2
+    submenu_item_2 = SubmenuItem(color("Go to wireless options", fg='Green'), submenu=wireless_submenu)
+    submenu_item_2.set_menu(menu)
+
     # Add all the items to the root menu
     menu.append_item(FunctionItem(color("Select an organization", fg='Green'), select_organization))
     menu.append_item(FunctionItem(color("View the selected organization", fg='Green'), view_organization))
-    menu.append_item(FunctionItem(color("Go to wireless options", fg='Green'), wireless_options))
+    menu.append_item(submenu_item_2)
     menu.append_item(ExitItem(color("Exit program", fg='Green')))
-    
+
     # Show the menu
     menu.start()
     menu.join()
